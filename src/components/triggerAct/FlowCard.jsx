ee728ad01488dc84dd28782ba943cbc9afb801af
Yash Lekhwani <yashlekhwani49@gmail.com>
2025-11-03 22:34:14 +0530

    final fix

--- src/components/triggerAct/FlowCard.jsx
+++ src/components/triggerAct/FlowCard.jsx
@@ -1,7 +1,7 @@
 const FlowCard = ({ title, description, image }) => {
   return (
     <div className="bg-white border border-gray-200 rounded-2xl shadow-md p-6 w-full max-w-sm hover:shadow-lg transition-all duration-300">
-      <h3 className="text-lg font-semibold bg-linear-to-r from-blue-600 to-purple-600 bg-clip-text text-transparent mb-2 mr-40">
+      <h3 className="text-lg font-semibold bg-linear-to-r from-blue-600 to-purple-600 bg-clip-text text-transparent mb-2 lg:mr-40">
         {title}
       </h3>
 
